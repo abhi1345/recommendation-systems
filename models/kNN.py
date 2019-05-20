83d552bb41ab4050d30c01b2374f8e1670c8f246
abhi1345 <sh_abhi@ymail.com>
2019-05-20 02:35:00 -0700

    Content Based Car Recommendations

--- models/kNN.py
+++ models/kNN.py
@@ -1,26 +1,31 @@
+#K Nearest Neighbors Classifier
+
 class kNearestNeighbors:
     def __init__(self):
         pass
-    
-    def fit(self, X, y):
+
+    def fit(self, X):
         self.X = X
-        self.y = y
-    
+
     def distance(self, u, v):
         #Euclidean
         s = 0
         for i in range(len(u)):
             s += (u[i] - v[i])**2
         return s**(0.5)
-    
-    def classify(self, z):
-        min_dist = float('inf')
-        answer = 0
+
+    def classify(self, z, n=1):
+        heap = []
+        heapmax = float("inf")
         for i in range(len(self.X)):
             x = self.X[i]
-            yhat = self.y[i]
             d = self.distance(x, z)
-            if d < min_dist:
-                min_dist = d
-                answer = yhat
-        return answer
+            if heap:
+                hmax = max(heap, key=lambda x: x[0])
+                hmax_d = hmax[0]
+            if len(heap) < n:
+                heap.append((d, i))
+            elif d < hmax_d:
+                heap.remove(hmax)
+                heap.append((d, i))
+        return heap
